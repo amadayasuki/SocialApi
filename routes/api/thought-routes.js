598a139f3f0c71416005c041523e9d8e4d7e3655
Yentzer Sensess <sensessyentzer@outlook.com>
2022-08-08 20:13:32 -0400

    Thought-Routes Update--still needs to be edited

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,18 +1,32 @@
+//Still need to edit
+
 const router = require('express').Router();
 const {
-
+    
 } = require('../../controllers/comment-controller');
 
 // /api/thought
 router
+    .route('/')
+    .get(getAllThoughts)
+    .post(addThought);
 
 // /api/thought/thoughtID
 router
   .route('/:pizzaId/:commentId')
-  .put(addReply)
-  .delete(removeComment);
+  .put(updateThought)
+  .get(getThoughtById)
+  .delete(removeThought);
 
+// /api/thought/reactions
+router
+    .route('/:thoughtId')
+    .post(addReaction);
+    
 
-router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
+// /api/thought/thoughtID/reaction/reactionId
+router
+.route('/:thoughtId')
+.delete(removeReaction);
 
 module.exports = router;
